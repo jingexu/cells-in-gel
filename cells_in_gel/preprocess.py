1fd7c2a35cafa132374d2694be9175e76d7edd64
Jenny Bennett <jenniferbennett020@gmail.com>
2020-05-25 19:50:45 -0700

    Updated the label function

--- cells_in_gel/preprocess.py
+++ cells_in_gel/preprocess.py
@@ -3,38 +3,93 @@ import numpy as np
 from scipy import ndimage
 
 from skimage import img_as_ubyte
-from skimage.filters import threshold_triangle, rank, laplace
-from skimage.segmentation import clear_border
+from skimage.exposure import adjust_sigmoid
+from skimage.filters import threshold_otsu, rank, laplace
 from skimage.measure import label
-from skimage.morphology import closing, square, remove_small_objects
+from skimage.morphology import square, remove_small_objects
 from skimage.color import label2rgb
 from skimage.transform import rescale
 
 
-def colorize(image, i, x, min_size=250, connectivity=1):
+def frequency_filter(im, mu, sigma, passtype='low'):
+    '''
+    This function applies a lowpass or highpass filter to an image.
+
+    Paramters
+    ---------
+    im : (N, M) ndarray
+        Grayscale input image.
+    mu : float, optional
+        Average for input in low pass filter. Default value is 500.
+    sigma : float, optional
+        Standard deviation for input in low pass filter. Default value is 70.
+    passtype: string
+        Applies a 'high' or 'low' pass filter. Default value is 'low'.
+
+    Returns
+    -------
+    out : ndarray
+        Low or high pass filtered output image.
+
+    Examples
+    --------
+    >>> image = plt.imread('C3-NTG-CFbs_NTG5ECM_1mMRGD_20x_003.tif')
+    >>> lowpass = frequency_filter(im, 500, 70, passtype='low')
+    '''
+    # define x and y based on image shape
+    y_length, x_length = np.shape(im)
+    xi = np.linspace(0, x_length-1, x_length)
+    yi = np.linspace(0, y_length-1, y_length)
+    x, y = np.meshgrid(xi, yi)
+
+    # define lowpass or highpass filter
+    if passtype == 'low':
+        gfilt = np.exp(-((x-mu)**2 + (y-mu)**2)/(2*sigma**2))
+    if passtype == 'high':
+        gfilt = 1 - np.exp(-((x-mu)**2 + (y-mu)**2)/(2*sigma**2))
+
+    fim = np.fft.fft2(im)  # moving to spacial domain
+    fim_c = np.fft.fftshift(fim)  # centering
+    fim_filt = np.multiply(fim_c, gfilt)  # apply the filter
+    fim_uc = np.fft.ifftshift(fim_filt)  # uncenter
+    im_pass = np.real(np.fft.ifft2(fim_uc))  # perform inverse transform
+
+    return im_pass
+
+
+def phalloidin_labeled(im, x, mu=500, sigma=70, cutoff=0, gain=100,
+                       min_size=250, connectivity=1):
     """
-    Signature: colorize(*args)
-    Docstring: segment and label image
+    Signature: phalloidin_labeled(*args)
+    Docstring: Segment and label image
 
     Extended Summary
     ----------------
-    The colorize function defines the threshold value for the desired image by
-    the triangle function and then creates a binarized image by setting pixel
-    intensities above that thresh value to white, and the ones below to black
-    (background). Next, it closes up the image by filling in random noise
-    within the cell outlines, smooths/clears out the border, and removes small
-    noise pixels from the background. It then labels adjacent pixels with the
-    same value and defines them as a region. It returns an RGB image with
-    color-coded labels.
+    The colorize function applies preprocessing filters (contrast and high
+    pass) then defines the threshold value for the desired image. Thresholding
+    is calculated by the otsu function creates a binarized image by setting
+    pixel intensities above that thresh value to white, and the ones below to
+    black (background). Next, it cleans up the image by filling in random noise
+    within the cell outlines and removes small background objects. It then
+    labels adjacent pixels with the same value and defines them as a region.
+    It returns an RGB image with color-coded labels.
 
-    Parameters
-    ----------
-    image : 2D array
-            greyscale image
-    i : int
-        The dimension of square to be used for binarization.
+    Paramters
+    ---------
+    im : (N, M) ndarray
+        Grayscale input image.
     x : float
         The dimension of image in microns according to imageJ.
+    cutoff : float, optional
+        Cutoff of the sigmoid function that shifts the characteristic curve
+        in horizontal direction. Default value is 0.
+    gain : float, optional
+        The constant multiplier in exponential's power of sigmoid function.
+        Default value is 100.
+    mu : float, optional
+        Average for input in low pass filter. Default value is 500.
+    sigma : float, optional
+        Standard deviation for input in low pass filter. Default value is 70.
     min_size : int, optional
         The smallest allowable object size. Default value is 250.
     connectivity : int, optional
@@ -43,43 +98,44 @@ def colorize(image, i, x, min_size=250, connectivity=1):
 
     Returns
     -------
-    out: plot of RGB image overlay (ndarray),
-        label_image (2D ndarray)
+    out : label_image (ndarray) segmented and object labeled for analysis
 
     Examples
     --------
     >>> image = plt.imread('C3-NTG-CFbs_NTG5ECM_1mMRGD_20x_003.tif')
-    >>> label_image = colorize(image, 4, 200, min_size=250, connectivity=1)
+    >>> label_image = phalloidin_488_binary(image, 200, mu=500, sigma=70,
+                                            cutoff=0, gain=100)
     """
-    # resizing image
-    image = rescale(image, x/1024, anti_aliasing=False)
+    # contrast adjustment
+    im_con = adjust_sigmoid(im, cutoff=cutoff, gain=gain, inv=False)
 
-    # applying threshold to image
-    thresh = threshold_triangle(image)
-    binary_holes = closing(image > thresh, square(i))
-    binary = ndimage.binary_fill_holes(binary_holes)
+    # contrast + low pass filter
+    im_lo = frequency_filter(im_con, mu, sigma, passtype='low')
 
-    # cleaning up boundaries of cells
-    cleared = clear_border(binary)
+    # contrast + low pass + binary
+    thresh = threshold_otsu(im_lo, nbins=256)
+    im_bin = im_lo > thresh
 
-    # remove small objects min_size
-    no_noise = remove_small_objects(cleared, min_size=min_size,
-                                    connectivity=connectivity,
-                                    in_place=False)
+    # fill holes and remove small objects
+    im_fill = ndimage.binary_fill_holes(im_bin)
+    im_clean = remove_small_objects(im_fill, min_size=min_size,
+                                    connectivity=connectivity, in_place=False)
 
     # labelling regions that are cells
-    label_image = label(no_noise)
+    label_image = label(im_clean)
 
     # coloring labels over cells
-    image_label_overlay = label2rgb(label_image, image=image, bg_label=0)
+    image_label_overlay = label2rgb(label_image, image=im, bg_label=0)
     print(image_label_overlay.shape)
 
+    # plot overlay image
     fig, ax = plt.subplots(figsize=(10, 6))
     ax.imshow(image_label_overlay)
 
     ax.set_axis_off()
     plt.tight_layout()
     plt.show()
+
     return (label_image)
 
 
